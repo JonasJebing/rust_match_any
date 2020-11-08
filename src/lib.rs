18c1167baeb8bd0e77d9927ab9e1297c9f38e4fb
Jonas Jebing <noreply.jonas.8049@protonmail.com>
2020-11-08 12:39:16 +0100

    added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,32 @@
+/// Matches an expression to any of the patterns and executes the same expression arm for any match.
+///
+/// # Examples
+///
+/// ```
+/// use match_any::match_any;
+/// let result: Result<i64, i32> = Err(42);
+/// let option: Option<i64> = match_any!(result, Ok(i) | Err(i) => Some(i.into()));
+/// assert_eq!(option, Some(42));
+/// ```
+///
+/// # Macro Expansion
+///
+/// ```
+/// use match_any::match_any;
+/// let result: Result<i32, i32> = Err(42);
+/// match_any!(result, Ok(i) | Err(i) => Some(i));
+/// ```
+/// expands to
+/// ```
+/// let result: Result<i32, i32> = Err(42);
+/// match result { Ok(i) => Some(i), Err(i) => Some(i) };
+/// ```
 #[macro_export]
 macro_rules! match_any {
-    ( $match_expr:expr , $( $pat:pat )|+ => $expr:expr ) => {
-        match $match_expr {
+    ( $expr:expr , $( $pat:pat )|+ => $expr_arm:expr ) => {
+        match $expr {
             $(
-                $pat => $expr,
+                $pat => $expr_arm,
             )+
         }
     };
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,7 +2,14 @@ use match_any::match_any;
 
 #[test]
 fn match_any_result() {
-    let result: Result<i32, i32> = Err(1);
-    let option: Option<i32> = match_any!(result, Ok(i) | Err(i) => Some(i));
+    let result: Result<i32, i64> = Err(1);
+    let option: Option<i64> = match_any!(result, Ok(i) | Err(i) => Some(i.into()));
     assert_eq!(option, Some(1));
+}
+
+#[test]
+fn unnecessary_match_any_ok() {
+    let a = 30;
+    let x = match_any!(a, ref b => b);
+    assert_eq!(*x, a);
 }
